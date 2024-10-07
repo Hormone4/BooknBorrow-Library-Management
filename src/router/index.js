36c25a42ae0a9c426cc2641e4b85ee09fd69a6eb
Eliot999 <71841438+Eliot999@users.noreply.github.com>
2024-10-07 09:56:09 +0200

    added library table
    Does not support modifs yet but can display the table accurately

--- src/router/index.js
+++ src/router/index.js
@@ -3,28 +3,41 @@ import Router from 'vue-router'
 
 import HelloWorld from '@/components/HelloWorld'
 import BooksModule from '@/components/BooksModule'
+import LibraryModule from '@/components/LibraryModule' // Import the LibraryModule component
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    // Home Route
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
     },
 
+    // Books Module Routes
     {
       path: '/books',
-      redirect: '/books/list/all'
+      redirect: '/books/list/all' // Redirect to the books list
     },
-
     {
       path: '/books/:action/:id',
       name: 'books',
       component: BooksModule,
       props: true
-    }
+    },
 
+    // Libraries Module Routes
+    {
+      path: '/libraries',
+      redirect: '/libraries/list/all' // Redirect to the libraries list
+    },
+    {
+      path: '/libraries/:action/:id',
+      name: 'libraries',
+      component: LibraryModule,
+      props: true
+    }
   ]
 })
