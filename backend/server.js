3c1553adb07cd585944ace9e16514b185ebc3240
Eliot SIMON <71841438+Eliot999@users.noreply.github.com>
2024-11-06 10:36:01 +0100

    fixed backend error, can now run

--- backend/server.js
+++ backend/server.js
@@ -50,6 +50,8 @@ app.get('/', (request, response) => { // 'GET' or 'POST'...
 
 // setup additional routes
 // app.use(routeBase, callback);
-app.use("/static", express.static(__dirname + '/static'));
-app.use("/booknborrowapi", require("./controllers/booksapi.route"));
+
+//app.use("/static", express.static(__dirname + '/static'));
+//app.use("/booknborrowapi", require("./controllers/booksapi.route"));
+
 // app.use("/auth", require("./controllers/auth.route"));
