f5d589162dbf3ca210e4daf55c36c87254a25659
Eliot SIMON <71841438+Eliot999@users.noreply.github.com>
2024-11-11 12:07:58 +0100

    corrected endpoints for books route

--- backend/controllers/booksapi.route.js
+++ backend/controllers/booksapi.route.js
@@ -8,11 +8,11 @@ const bookRepo = require('../utils/books.repository');
 
 
 
-router.get('/books/brands', authorListAction);
-router.get('/books/list', bookListAction); //Works
-router.get('/books/show/:bookId', bookShowAction); // works
-router.get('/books/del/:bookId', bookDelAction);
-router.post('/books/update/:bookId', bookUpdateAction);
+router.get('/brands', authorListAction);
+router.get('/list', bookListAction); //Works
+router.get('/show/:bookId', bookShowAction); // works
+router.get('/del/:bookId', bookDelAction);
+router.post('/update/:bookId', bookUpdateAction);
 
 // http://localhost:9000/booksapi/brands
 async function authorListAction(request, response) {
