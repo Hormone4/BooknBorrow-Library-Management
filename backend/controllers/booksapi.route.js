483207ce4bd44b923730c7a237e82b71bca7d78c
Hormone <sach.laur.04@protonmail.com>
2024-11-12 15:35:29 +0100

    fixed SQL requests in book Repo

--- backend/controllers/booksapi.route.js
+++ backend/controllers/booksapi.route.js
@@ -12,7 +12,7 @@ const bookRepo = require('../utils/books.repository');
 router.get('/list', bookListAction); //Works
 router.get('/show/:bookId', bookShowAction); // works
 router.get('/del/:bookId', bookDelAction); // need to add check to see if no book_id is used as foreign key
-router.post('/update/:bookId', bookUpdateAction);
+router.post('/update/:bookId', bookUpdateAction); // post means that it is a form submission
 
 
 
@@ -30,12 +30,19 @@ async function bookDelAction(request, response) {
     response.send(JSON.stringify(result));
 }
 async function bookUpdateAction(request, response) {
-    // var json = JSON.stringify(request.body); // bodyParser can process json in body + regular POST form input too
+    //var json = JSON.stringify(request.body); // bodyParser can process json in body + regular POST form input too
     // console.log(json);
     // TODO: !!! INPUT VALIDATION !!!
     var bookId = request.params.bookId;
-    if (bookId==="0") bookId = await bookRepo.addOneBook(request.body.book_brand);
-    var isFancy = (request.body.book_isFancy === undefined || request.body.book_isFancy === false) ? 0 : 1;
+    if (bookId==="0") {
+        bookId = await bookRepo.addOneBook(
+            request.body.book_name,
+            request.body.book_author,
+            request.body.book_description,
+            request.body.book_publicationDate,
+            request.body.book_isbn,
+            request.body.book_imageFileName);
+    }
     var numRows = await bookRepo.editOneBook(bookId,
         request.body.book_name,
         request.body.book_author,
