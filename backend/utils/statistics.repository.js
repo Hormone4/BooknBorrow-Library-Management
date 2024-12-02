6eb1d1ece3038a518a9cfb0b72d1b3e4e9d42e7b
Eliot999 <71841438+Eliot999@users.noreply.github.com>
2024-12-02 17:39:39 +0100

    added file statisctics repo

---
+++ backend/utils/statistics.repository.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const os = process.env.OS;
+let path = __dirname + "\\db.include.js";
+if (os === 'l') {
+    path = __dirname + "/db.include.js";  // Linux path
+} else if (os === 'w') {
+    path = __dirname + "\\db.include.js";  // Windows path
+}
+pool = require(path)
+const { verifyInput } = require('../utils/inputvalidation');
+
+module.exports = {
+
+    
+}
